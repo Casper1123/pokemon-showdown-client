ff522a73dd2bf39c190c51d12f18babf8009f2eb
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-08-11 18:13:32 +0200

    Update servers-example.inc.php

--- config/servers-example.inc.php
+++ config/servers-example.inc.php
@@ -1,11 +1,18 @@
 <?php
 
 $PokemonServers = array (
-  'showdown' => 
+  'casper1123' =>
   array (
-    'name' => 'Smogon University',
-    'id' => 'showdown',
-    'server' => 'sim.psim.us',
-    'port' => 8000,
+    'name' => 'Casper1123\'s Custom Showdown',
+    'id' => 'casper1123',
+    'server' => 'showdownserver.casper1123.nl',
+    'port' => 443,
   ),
+  'showdown' =>
+    array (
+      'name' => 'Smogon University',
+      'id' => 'showdown',
+      'server' => 'sim.psim.us',
+      'port' => 8000,
+    ),
 );
