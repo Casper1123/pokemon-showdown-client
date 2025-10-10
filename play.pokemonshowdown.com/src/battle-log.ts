a78a8b24b51c0b9d45cb8f9cb36c048671ddb781
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-10-10 14:45:11 +0200

    Reapply "Merge branch 'smogon-master'"
    This reverts commit 0e7e39f9de5cc3bf2188be4802487a5d54ef2052.

--- play.pokemonshowdown.com/src/battle-log.ts
+++ play.pokemonshowdown.com/src/battle-log.ts
@@ -1843,7 +1843,7 @@ export class BattleLog {
 }
 
 if (window.Net) {
-	Net(`/config/colors.json`).get().then(response => {
+	Net(`/config/colors.json?${Math.random()}`).get().then(response => {
 		const data = JSON.parse(response);
 		Object.assign(Config.customcolors, data);
 	}).catch(() => {});
