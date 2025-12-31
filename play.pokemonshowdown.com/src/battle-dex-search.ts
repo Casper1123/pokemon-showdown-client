0ad96eb37029a3dd174614493531f97e5228a0b8
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-12-31 03:50:30 +0100

    changed filtering doubles format.

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -1098,8 +1098,7 @@ class BattlePokemonSearch extends BattleTypedSearch<'pokemon'> {
 		let tierSet: SearchRow[] = table.tierSet;
 		let slices: { [k: string]: number } = table.formatSlices;
 		if (this.formatType === 'natdexcustom') {
-			if (!this.isDoubles) tierSet = tierSet.slice(slices["MOD Uber"]);
-			else tierSet = tierSet.slice(slices["DMOD"]);
+			tierSet = tierSet.slice(slices["MOD Uber"]);
 		} else if (format === 'ubers' || format === 'uber' || format === 'ubersuu' || format === 'nationaldexdoubles') {
 			tierSet = tierSet.slice(slices.Uber);
 		} else if (isVGCOrBS || (isHackmons && dex.gen === 9 && !this.formatType)) {
